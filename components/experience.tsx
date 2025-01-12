0fe72e77cd76c5ca2092255f1762ba52102d1966
Syahmim <syahmimcbs600@gmail.com>
2025-01-13 07:08:03 +0800

    Updated experience and changed icons layout

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import React from "react";
+import { useTheme } from "@/context/theme-context";
+import { experiencesData } from "@/lib/data";
+import type { Experience } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
 import SectionHeading from "./section-heading";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { experiencesData } from "@/lib/data";
-import { useSectionInView } from "@/lib/hooks";
-import { useTheme } from "@/context/theme-context";
+import Image from "next/image";
+import React from "react";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience", 0.5);
@@ -18,39 +20,62 @@ export default function Experience() {
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
-        {experiencesData.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              visible={true}
-              contentStyle={{
-                background:
-                  theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
-                border: "1px solid rgba(0,0,0,0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{ borderRight: theme === 'light' ?  "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255.0.5)" }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{ background: theme === "light" ? "white" : "rgba(255,255,255,0.15)", fontSize: "1.5rem" }}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                {item.description}
-              </p>
-              <ul className="flex flex-wrap mt-4 gap-2">
-                {item.tags.map((tag, index) => (
-                  <li
-                    className="bg-black/[0.7] px-3 py-2 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                    key={index}
-                  >
-                    {tag}
-                  </li>
-                ))}
-              </ul>
-            </VerticalTimelineElement>
-          </React.Fragment>
+        {experiencesData.map((item: Experience, index: number) => (
+          <VerticalTimelineElement
+            key={index}
+            contentStyle={{
+              background:
+                theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
+              border: "1px solid rgba(0,0,0,0.05)",
+              textAlign: "left",
+              padding: "1.3rem 2rem",
+            }}
+            contentArrowStyle={{
+              borderRight:
+                theme === "light"
+                  ? "0.4rem solid #9ca3af"
+                  : "0.4rem solid rgba(255,255,255.0.5)",
+            }}
+            date={item.date}
+            icon={
+              <div className="icon-wrapper">
+                <Image
+                  src={item.icon}
+                  alt={item.location}
+                  layout="intrinsic"
+                  width={64} /* Matches 4rem = 64px */
+                  height={64} /* Matches 4rem = 64px */
+                  objectFit="cover"
+                />
+              </div>
+            }
+            iconStyle={{
+              background:
+                theme === "light" ? "white" : "rgba(255,255,255,0.15)",
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "4rem",
+              height: "4rem",
+            }}
+          >
+            <h3 className="font-semibold capitalize">{item.title}</h3>
+            <p className="font-normal !mt-0">{item.location}</p>
+            <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+              {item.description}
+            </p>
+            <ul className="flex flex-wrap mt-4 gap-2">
+              {item.tags?.map((tag, index) => (
+                <li
+                  className="bg-black/[0.7] px-3 py-2 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                  key={index}
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </VerticalTimelineElement>
         ))}
       </VerticalTimeline>
     </section>
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,13 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import recipe from "@/public/recipe.png";
+import { StaticImageData } from "next/image";
 import gym from "@/public/gym.png";
 import ascenda from "@/public/ascenda.png";
 import dota from "@/public/dota.png";
+import SchneiderElectricLogo from "@/public/schneider.png";
+import LumensLogo from "@/public/lumens.png";
+import CreditAgricoleLogo from "@/public/cacib.png";
 
 export const links = [
   {
@@ -33,13 +37,30 @@ export const links = [
   },
 ] as const;
 
-export const experiencesData = [
+export type Experience = {
+  title: string;
+  location: string;
+  icon: StaticImageData;
+  date: string;
+  description?: string;
+  tags?: string[];
+};
+
+export const experiencesData: Experience[] = [
+  {
+    title: "Software Engineer Intern",
+    location: "Crédit Agricole CIB",
+    icon: CreditAgricoleLogo,
+    date: "Jan 2025 - Present",
+    description: "In progress",
+    tags: ["C#", ".NET", "Typescript", "Playwright", "Angular"],
+  },
   {
     title: "Software Developer Intern",
     location: "Schneider Electric",
     description:
       "Collaborated with a team of engineers to implement new features, created 10 test cases, and executed 90+ verification tests. Detected and resolved bugs, ensuring precise alignment with product and company requirements. Automated, refactored and improved testing and deployment processes by leveraging Jenkins and existing software tools, reducing manual intervention and increasing overall efficiency.",
-    icon: React.createElement(CgWorkAlt),
+    icon: SchneiderElectricLogo,
     date: "May 2024 - Dec 2024",
     tags: ["Python", "Testing", "Automation", "Jenkins"],
   },
@@ -48,11 +69,11 @@ export const experiencesData = [
     location: "Lumens",
     description:
       "Developed dynamic and interactive web applications using React for the front end and Express with Node.js for the back end. Utilised PostgreSQL as the primary database to ensure efficient and reliable data management. Collaborated closely with developers and end-users to gather, prioritise, and refine product requirements. This process encompassed advanced feature design, intuitive user interfaces, and robust technical specifications, resulting in tailored software solutions that addressed specific market needs and elevated user satisfaction.",
-    icon: React.createElement(FaReact),
+    icon: LumensLogo,
     date: "Jun 2023 - Aug 2023",
     tags: ["React", "Express", "Node.js", "PostgreSQL"],
   },
-] as const;
+];
 
 export const projectsData = [
   {
